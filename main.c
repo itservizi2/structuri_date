2e6da32ce0204ce0da76e8036e990755326d94ef
itservizi2 <itservizi.helpdesk@gmail.com>
2023-10-04 17:50:01 +0300

    1st C commit with print messages

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Hello! This is message 1.\n");
+    printf("Welcome! This is message 2.\n");
+    printf("Hi! This is message 3.\n");
+    printf("Greetings! This is message 4.\n");
+    printf("Hey! This is message 5.\n");
+    return 0;
+}
